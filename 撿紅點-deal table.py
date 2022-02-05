0e3d853ff55a24fdb3ff4bb9aba224882851004f
luying <91367098+lu1hOAO@users.noreply.github.com>
2022-02-05 10:09:50 +0800

    Create 撿紅點-deal table.py

---
+++ 撿紅點-deal table.py
@@ -0,0 +1,28 @@
+'''
+這個函數是在執行玩家出玩牌後從牌堆抽出一張排放到桌上的過程:
+如果抽出的牌和原本桌上的牌能配對，就必須湊成一對放到玩家的計分牌當中
+若無法湊成一對，就必須放在桌上
+流程如下:
+1.先從牌堆抽出一張牌儲存於temp
+2.依temp的屬性判別是否和桌上的牌湊成堆
+'''
+def dealtable(gamerbox):
+    temp=card.pop()
+    tempoint=temp%13+1
+    for i in table:
+        tablepoint=i%13+1
+        tablesuit=i//13
+        if tempoint<10:
+            if tempoint+tablepoint==10 :
+               gamerbox.append(i)
+               gamerbox.append(temp)
+               table.pop(table.index(i))     
+            else:
+                table.append(temp)   
+        else:
+            if tempoint==tablepoint:
+               gamerbox.append(i)
+               gamerbox.append(temp)
+               table.pop(table.index(i)) 
+            else:
+               table.append(temp)
